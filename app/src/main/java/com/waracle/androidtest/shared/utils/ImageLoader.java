570399f231497a0d695a211b164db463b1b1567f
Lukáš Vyletel <vyletel@gmail.com>
2018-02-15 22:19:29 +0100

    Refactor ImageLoader to download images on a  background thread

--- app/src/main/java/com/waracle/androidtest/shared/utils/ImageLoader.java
+++ app/src/main/java/com/waracle/androidtest/shared/utils/ImageLoader.java
@@ -2,15 +2,21 @@ package com.waracle.androidtest.shared.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
+
+import com.waracle.androidtest.shared.core.Failable;
+import com.waracle.androidtest.shared.core.IO;
+import com.waracle.androidtest.shared.core.Transform;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 /**
  * Created by Riad on 20/05/2015.
@@ -27,23 +33,58 @@ public class ImageLoader {
      * @param url       image url
      * @param imageView view to set image too.
      */
-    public void load(String url, ImageView imageView) {
+    public void load(@NonNull String url, @NonNull final ImageView imageView) {
         if (TextUtils.isEmpty(url)) {
             throw new InvalidParameterException("URL is empty!");
         }
 
-        // Can you think of a way to improve loading of bitmaps
-        // that have already been loaded previously??
+        getBitmapIO(url).runAsync(new IO.IOCallback<Failable<Bitmap>>() {
+            @Override
+            public void callback(@NonNull Failable<Bitmap> value) {
+                value.fold(new Failable.FailableFoldCallback<Bitmap>() {
+                    @Override
+                    public void foldValue(@NonNull Bitmap value) {
+                        setImageView(imageView, value);
+                    }
 
-        try {
-            setImageView(imageView, convertToBitmap(loadImageData(url)));
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage());
-        }
+                    @Override
+                    public void foldError(@NonNull String error) {
+                        Log.e(TAG, error);
+                    }
+                });
+            }
+        });
     }
 
-    private static byte[] loadImageData(String url) throws IOException {
-        HttpURLConnection connection = UrlConnectionUtils.createConnection(url);
+    private IO<Failable<Bitmap>> getBitmapIO(@NonNull String url) {
+        return loadImageData(url).map(new Transform<Failable<byte[]>, Failable<Bitmap>>() {
+            @Override
+            public Failable<Bitmap> transform(Failable<byte[]> input) {
+                return input.map(new Transform<byte[], Bitmap>() {
+                    @Override
+                    public Bitmap transform(byte[] input) {
+                        return convertToBitmap(input);
+                    }
+                });
+            }
+        });
+    }
+
+    private static IO<Failable<byte[]>> loadImageData(@NonNull final String url) {
+        return new IO<>(new IO.IOOperation<Failable<byte[]>>() {
+            @NonNull
+            @Override
+            public Failable<byte[]> doIOOperation() {
+                try {
+                    return doHttpCall(UrlConnectionUtils.createConnection(url));
+                } catch (IOException exception) {
+                    return new Failable<>(exception.getMessage());
+                }
+            }
+        });
+    }
+
+    private static @NonNull Failable<byte[]> doHttpCall(@NonNull HttpURLConnection connection) {
         InputStream inputStream = null;
         try {
             try {
@@ -57,7 +98,9 @@ public class ImageLoader {
             // Can you think of a way to make the entire
             // HTTP more efficient using HTTP headers??
 
-            return StreamUtils.readUnknownFully(inputStream);
+            return new Failable<>(StreamUtils.readUnknownFully(inputStream));
+        } catch (IOException exception) {
+            return new Failable<>(exception.getMessage());
         } finally {
             // Close the input stream if it exists.
             StreamUtils.close(inputStream);
